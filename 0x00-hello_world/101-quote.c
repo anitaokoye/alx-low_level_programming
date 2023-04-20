dcbb6b0cc54a4361dad31334a936904b3dfa9ed2
anitaokoye <anita.larryokoye@gmail.com>
2023-04-20 15:54:54 -0700

    qoute

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main -A program that will return 1
+ * main - Entry
  * Return: 1 (success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
