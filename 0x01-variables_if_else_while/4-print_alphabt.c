70fe4aa1d4645bbbe6f9ed0171459eabc67a5b15
anitaokoye <anita.larryokoye@gmail.com>
2023-04-23 08:21:02 -0700

    lower and upper case

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - A program that print alphabet in lowercase
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
