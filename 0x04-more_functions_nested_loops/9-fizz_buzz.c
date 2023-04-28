8555512907c402408365d36461935edc357abf39
anitaokoye <anita.larryokoye@gmail.com>
2023-04-28 06:21:35 -0700

    fizz buz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Funtions to print a number from 1 to 100 followed by new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && !(i % 5 == 0))
+		{
+			printf("Fizz");
+		}
+	else if (i % 5 == 0 && !(i % 3 == 0))
+	{
+		printf("Buzz");
+	}
+	else if (i % 3 == 0 && i % 5 == 0)
+	{
+		printf("FizzBuzz");
+	}
+	else
+	{
+		printf("%d", i);
+	}
+	if (i != 100)
+	{
+		printf(" ");
+	}
+	else
+		printf("\n");
+	}
+	return (0);
+}
+
