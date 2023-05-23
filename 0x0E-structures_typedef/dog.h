247f331c8bf90d73ca005b4944b1972828bc0036
anitaokoye <anita.larryokoye@gmail.com>
2023-05-23 14:02:07 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,16 +2,16 @@
 #define DOG_H
 
 
-	/**
-	 * struct dog - a dog's basic info
-	 * @name: First member
-	 * @age: Second member
-	 * @owner: Third member
-	 *
-	 * Description: Longer description
-	 */
-	struct dog
-	{
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
 		char *name;
 		float age;
 		char *owner;
@@ -32,4 +32,4 @@
 	int _strlen(char *s);
 
 
-	#endif
+#endif
